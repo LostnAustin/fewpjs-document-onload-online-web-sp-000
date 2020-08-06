c6b632e27c94e3cdacba480b27d1069b442f47ce
Chad Lengefeld <chadlengefeld@gmail.com>
2020-08-06 08:35:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Your code goes here
 document.addEventListener('DOMContentLoaded', function() {
-  const text = document.getElementById('text');
-  innerHTML = 'This is really cool!';
+  document.getElementById('text'.innerHTML = 'This is really cool!');
 });
