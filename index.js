00cf6c4012834c07e230026730da20d3b886d329
Chad Lengefeld <chadlengefeld@gmail.com>
2020-08-06 08:05:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener('DOMContentLoaded', function() {
-  innerHTML = 'This is really cool!';
+  innerHTML = 'This is really cool! JavaScript is so cool. It lets me add text to my page programmatically.';
   console.log('The DOM has loaded');
 });
